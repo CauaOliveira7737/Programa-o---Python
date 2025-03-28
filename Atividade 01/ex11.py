da83ca129bc833527716af3a9618d685b1f8535c
CauaOliveira7737 <161508389+CauaOliveira7737@users.noreply.github.com>
2025-03-28 14:23:19 -0300

    Create ex11.py

---
+++ Atividade 01/ex11.py
@@ -0,0 +1,7 @@
+nome = input("Insira seu nome: ")
+senha = input("Insira sua senha: ")
+
+if nome == senha:
+    print("O nome e a senha não podem ser iguais.")
+else:
+    print("SUCESSO! Login bem-sucedido!")
