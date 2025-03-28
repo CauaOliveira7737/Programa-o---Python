0c7640893c0459586dcf972295e66fae4b66891b
CauaOliveira7737 <161508389+CauaOliveira7737@users.noreply.github.com>
2025-03-28 14:04:49 -0300

    Create ex09.py

---
+++ Atividade 01/ex09.py
@@ -0,0 +1,9 @@
+num1 = float(input("Insira o primeiro número: "))
+num2 = float(input("Insira o segundo número: "))
+num3 = float(input("Insira o terceiro número: "))
+
+maior_numero = max(num1, num2, num3)
+menor_numero = min(num1, num2, num3)
+
+print(f"O maior número é: {maior_numero}")
+print(f"O menor número é: {menor_numero}")
